c9324383fbe999a2010e4574be0e83351ed6e407
QuentinC <webmaster@quentinc.net>
2019-05-24 21:29:09 +0200

    Begin preferences dialog

--- PreferencesDlg.cpp
+++ PreferencesDlg.cpp
@@ -3,6 +3,7 @@
 #include "MainWindow.hpp"
 #include <wx/listctrl.h>
 #include <wx/listbook.h>
+#include <wx/spinctrl.h>
 #include "cpprintf.hpp"
 #include "stringUtils.hpp"
 #include "UniversalSpeech.h"
@@ -20,6 +21,44 @@ wxDialog(parent, -1, U(translate("PreferencesDlg")) ),
 app(app)
 {
 book = new wxListbook(this, 4999, wxDefaultPosition, wxDefaultSize, wxLB_LEFT);
+
+{ // Integration page
+auto page = new wxPanel(book);
+auto sizer = new wxBoxSizer(wxVERTICAL);
+wxString openActions[] = { U(translate("PrefIntegrationOAOpen")), U(translate("PrefIntegrationOAAppend")) };
+auto openAction = new wxRadioBox(page, 300, U(translate("PrefIntegrationOA")), wxDefaultPosition, wxDefaultSize, 2, openActions);
+auto openFocus = new wxCheckBox(page, 301, U(translate("PrefIntegrationOAFocus")));
+sizer->Add(openAction, 1, wxEXPAND);
+sizer->Add(openFocus);
+page->SetSizer(sizer);
+book->AddPage(page, U(translate("PrefIntegrationPage")));
+}
+
+{ // Audio page
+auto page = new wxPanel(book);
+auto sizer = new wxBoxSizer(wxVERTICAL);
+auto includeLoopback = new wxCheckBox(page, 325, U(translate("PrefAudioIncludeLoopback")));
+sizer->Add(includeLoopback);
+page->SetSizer(sizer);
+book->AddPage(page, U(translate("PrefAudioPage")));
+}
+
+{ // Casting page
+auto page = new wxPanel(book);
+auto castAutoTitle = new wxCheckBox(page, 351, U(translate("PrefCastingAutoTitle")));
+auto lblLRTB = new wxStaticText(page, wxID_ANY, U(translate("PrefCastingLRTB")) );
+auto spLRT = new wxSpinCtrl(page, 350, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 1, 300, 60);
+auto lblLRTA = new wxStaticText(page, wxID_ANY, U(translate("PrefCastingLRTA")) );
+auto sizer = new wxBoxSizer(wxVERTICAL);
+auto subsizer = new wxBoxSizer(wxHORIZONTAL);
+subsizer->Add(lblLRTB);
+subsizer->Add(spLRT);
+subsizer->Add(lblLRTA);
+sizer->Add(castAutoTitle);
+sizer->Add(subsizer);
+page->SetSizer(sizer);
+book->AddPage(page, U(translate("PrefCastingPage")));
+}
 
 //auto lblInfo = new wxStaticText(this, wxID_ANY, U(translate("ItInfoList")) );
 //lcInfo = new wxListView(this, 501, wxDefaultPosition, wxDefaultSize, wxLC_NO_HEADER | wxLC_SINGLE_SEL | wxLC_REPORT);

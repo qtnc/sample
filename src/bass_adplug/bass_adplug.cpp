e2ef4889047f383d928dbd9fb66157bd56ac58b7
QuentinC <webmaster@quentinc.net>
2023-01-29 13:16:07 +0100

    Adding AdPlug input plugin (WIP)

--- src/bass_adplug/bass_adplug.cpp
+++ src/bass_adplug/bass_adplug.cpp
@@ -1,5 +1,6 @@
 #include "adplug.h"
 #include "emuopl.h"
+# include "binstr.h"
 #include "../common/bass.h"
 #include "../common/bass-addon.h"
 #ifndef bassfunc
@@ -15,15 +16,33 @@ CPlayer* player;
 CEmuopl* emuopl;
 CFileProvider* fs;
 DWORD handle;
+DWORD remaining;
+DWORD lengthMs;
+bool useFloat;
+std::string title, author, description;
 
 AdplugStream (
 CPlayer* player0, CEmuopl* emuopl0, CFileProvider* fs0): 
-player(player0), emuopl(emuopl0), fs(fs0), handle(0)
+player(player0), emuopl(emuopl0), fs(fs0), handle(0), remaining(0), lengthMs(0), useFloat(false)
  {}
 ~AdplugStream () {
 delete player;
 delete emuopl;
 delete fs;
+}
+
+};//AdplugStream
+
+struct BufferFileProvider: CFileProvider {
+char* buffer;
+int length;
+BufferFileProvider (char* b, int l): buffer(b), length(l) {}
+virtual ~BufferFileProvider () { delete buffer; }
+  virtual binistream *open(std::string) const {
+return new binisstream(buffer, length);
+}
+  virtual void close(binistream *b) const {
+if (b) delete b;
 }
 };
 
@@ -35,8 +54,19 @@ delete stream;
 }
 
 static DWORD CALLBACK StreamProc(HSTREAM handle, BYTE *buffer, DWORD length, AdplugStream *stream) {
-//##todo
-return length;
+if (stream->remaining<=0) {
+if (!stream->player->update()) {
+stream->player->rewind();
+return BASS_STREAMPROC_END;
+}
+stream->remaining = 4 * 48000/stream->player->getrefresh();
+}
+DWORD mult = stream->useFloat? 2 : 1;
+DWORD n = std::min(length/mult, stream->remaining);
+stream->remaining -= n;
+stream->emuopl->update( (short*)buffer, n/4);
+if (stream->useFloat) bassfunc->data.Int2Float(buffer, (float*)buffer, length/mult, 2);
+return n*mult;
 }
 
 static int readFully (BASSFILE file, void* buffer, int length) {
@@ -46,8 +76,23 @@ return pos;
 }
 
 static HSTREAM WINAPI StreamCreateProc(BASSFILE file, DWORD flags) {
+int unicode = 0;
+void* fnptr = bassfunc->file.GetFileName(file, &unicode);
+if (!fnptr) error(BASS_ERROR_FILEFORM);
+DWORD length = bassfunc->file.GetPos(file, BASS_FILEPOS_END);
+if (length<=0 || length>=2*1024*1024) error(BASS_ERROR_FILEFORM);
+char* buffer = new char[length];
+if (!buffer) error(BASS_ERROR_FILEFORM);
+readFully(file, buffer, length);
+
 std::string filename;
-CFileProvider* fsProvider = nullptr;
+if (unicode) {
+auto sptr = reinterpret_cast<const unsigned short*>(fnptr);
+while(sptr&&*sptr) filename += (char)*(sptr++);
+}
+else filename = static_cast<const char*>(fnptr);
+
+CFileProvider* fsProvider = new BufferFileProvider(buffer, length);
 CEmuopl* emuopl = new CEmuopl(48000, true, true);
 CPlayer* player = CAdPlug::factory(filename, emuopl, CAdPlug::players, *fsProvider);
 if (!player) {
@@ -55,14 +100,14 @@ delete emuopl;
 delete fsProvider;
 error(BASS_ERROR_FILEFORM);
 }
-/*if (!buffer || readFully(file, buffer, 1084)<4 || !checkModHeaders(buffer, 1084)) error(BASS_ERROR_FILEFORM);
-DWORD length = bassfunc->file.GetPos(file, BASS_FILEPOS_END);
-buffer = realloc(buffer, length);
-if (!buffer) error(BASS_ERROR_FILEFORM);
-readFully(file, buffer+1084, length -1084);*/
 bassfunc->file.Close(file);
 AdplugStream* stream = new AdplugStream(player, emuopl, fsProvider);
-flags &= BASS_SAMPLE_SOFTWARE|BASS_SAMPLE_LOOP|BASS_SAMPLE_3D|BASS_SAMPLE_FX 		|BASS_STREAM_DECODE|BASS_STREAM_AUTOFREE|0x3f000000; // 0x3f000000 = all speaker flags
+flags &= BASS_SAMPLE_FLOAT|BASS_SAMPLE_SOFTWARE|BASS_SAMPLE_LOOP|BASS_SAMPLE_3D|BASS_SAMPLE_FX 		|BASS_STREAM_DECODE|BASS_STREAM_AUTOFREE|0x3f000000; // 0x3f000000 = all speaker flags
+stream->useFloat = (flags&BASS_SAMPLE_FLOAT);
+stream->lengthMs = player->songlength();
+stream->title = player->gettitle();
+stream->author = player->getauthor();
+stream->description = player->getdesc();
 	stream->handle=bassfunc->CreateStream(48000, 2, flags, &StreamProc, stream, &funcs);
 	if (!stream->handle) { 
 delete stream;
@@ -112,28 +157,26 @@ info->origres = 16;
 
 static BOOL WINAPI AdplugCanSetPosition(AdplugStream *stream, QWORD pos, DWORD mode) {
 	if ((BYTE)mode!=BASS_POS_BYTE) error(BASS_ERROR_NOTAVAIL); // only support byte positioning (BYTE = ignore flags)
-//###todo
-//double dpos = pos / (48000.0 * 2 * (stream->useFloat? sizeof(float) : sizeof(short)) );
-//	if (dpos>=openmpt_module_get_duration_seconds(stream->mod)) error(BASS_ERROR_POSITION);
+double dpos = pos / (48000.0 * 2 * (stream->useFloat? sizeof(float) : sizeof(short)) );
+if (dpos>=stream->lengthMs*1000) error(BASS_ERROR_POSITION);
 	return TRUE;
 }
 
 static QWORD WINAPI AdplugSetPosition(AdplugStream* stream, QWORD pos, DWORD mode) {
-//###todo
-//double dpos = pos / (48000.0 * 2 * (stream->useFloat? sizeof(float) : sizeof(short)));
-//if (dpos<0 || dpos>=openmpt_module_get_duration_seconds(stream->mod)) errorn(BASS_ERROR_POSITION);
-//dpos = openmpt_module_set_position_seconds(stream->mod, dpos);
-//pos = dpos * (48000.0 * 2 * (stream->useFloat? sizeof(float) : sizeof(short)));
-//pos>>=4; pos<<=4;
+double dpos = pos / (48000.0 * 2 * (stream->useFloat? sizeof(float) : sizeof(short)));
+if (dpos<0 || dpos>=stream->lengthMs*1000) errorn(BASS_ERROR_POSITION);
+stream->player->seek(dpos*1000);
+pos = dpos * (48000.0 * 2 * (stream->useFloat? sizeof(float) : sizeof(short)));
+pos>>=4; pos<<=4;
 return pos;
 }
 
 static const char* WINAPI AdplugTags (AdplugStream* stream, DWORD type) {
 if (!stream) return NULL;
 switch(type){
-//case BASS_TAG_MUSIC_NAME: return str = openmpt_module_get_metadata(stream->mod, "title");
-//case BASS_TAG_MUSIC_AUTH: return str = openmpt_module_get_metadata(stream->mod, "artist");
-//case BASS_TAG_MUSIC_MESSAGE: return str = openmpt_module_get_metadata(stream->mod, "message_raw");
+case BASS_TAG_MUSIC_NAME: return stream->title.c_str();
+case BASS_TAG_MUSIC_AUTH: return stream->author.c_str();
+case BASS_TAG_MUSIC_MESSAGE: return stream->description.c_str();
 }
 //if (type>=BASS_TAG_MUSIC_INST && type<BASS_TAG_MUSIC_INST+openmpt_module_get_num_instruments(stream->mod)) {
 //return str = openmpt_module_get_instrument_name(stream->mod, type - BASS_TAG_MUSIC_INST);
@@ -166,7 +209,7 @@ static const BASS_PLUGINFORM frm[] = {
 };
 static BASS_PLUGININFO plugininfo = {0x02040A00, 1, frm };
 
-const void* WINAPI EXPORT BASSplugin(DWORD face) {
+extern "C" const void* WINAPI EXPORT BASSplugin(DWORD face) {
 switch (face) {
 		case BASSPLUGIN_INFO:
 			return (void*)&plugininfo;
@@ -176,7 +219,7 @@ switch (face) {
 	return NULL;
 }
 
-BOOL WINAPI EXPORT DllMain(HANDLE hDLL, DWORD reason, LPVOID reserved)
+extern "C" BOOL WINAPI EXPORT DllMain(HANDLE hDLL, DWORD reason, LPVOID reserved)
 {
 	switch (reason) {
 		case DLL_PROCESS_ATTACH:

00fec58775c5c26c08cb501a0158f2eb48fb08b7
QuentinC <webmaster@quentinc.net>
2025-09-05 10:47:29 +0200

    Update getdll.lua

--- getdll.lua
+++ getdll.lua
@@ -1,27 +1,34 @@
+local lfs = require('lfs')
 local dllpath = os.getenv('CPATH') .. '\\..\\dll64\\'
-local exe = arg[1]
+
 
 function table.find (t, x)
 for k, v in pairs(t) do
 if v==x then return k end
 end end
 
-local function dllList (fn, t)
-t = t or {}
-local dfp = io.popen('objdump -p ' .. fn .. ' | grep -i "DLL Name:"', 'r')
-for l in dfp:lines() do
-local dll = l:sub(12)
-if table.find(t, dll) then goto continue end
-local fr = io.open(dllpath..dll, 'rb')
-if not fr then goto continue end
+local function addToDLLs (dll, t)
+if table.find(t, dll) then return end
+local fr <close> = io.open(dllpath..dll, 'rb')
+if not fr then return end
 table.insert(t, dll)
-fr:close()
-::continue:: end
-dfp:close()
-return t
 end
 
-local dlls = dllList(exe)
+local function dllList (fn, t)
+t = t or {}
+local dfp <close> = io.popen('objdump -p ' .. fn .. ' | grep -i "DLL Name:"', 'r')
+for l in dfp:lines() do
+local dll = l:sub(12)
+addToDLLs(dll, t)
+end end
+
+local dlls = {}
+for i = 1, #arg do
+local dll = arg[i]
+if dll:lower():match('%.dll$') then addToDLLs(dll, dlls)
+else dllList(dll, dlls)
+end end
+
 do local i = 1 repeat
 local dll = dlls[i]
 dllList(dllpath..dll, dlls)
@@ -29,17 +36,18 @@ i = i+1
 until i > #dlls end
 
 for i, dll in ipairs(dlls) do
-local fw = io.open(dll, 'rb')
-if fw then 
-fw:close() 
-goto continue
-end
-local fr = io.open(dllpath..dll, 'rb')
+print(string.format('Check %s', dll))
+local curmd = lfs.attributes(dll, 'modification') or 0
+local defmd = lfs.attributes(dllpath..dll, 'modification') or 0
+local cursize = lfs.attributes(dll, 'size') or 0
+local defsize = lfs.attributes(dllpath..dll, 'size') or 0
+if curmd==defmd or cursize==defsize then goto continue end
+local src = curmd>defmd and dll or dllpath..dll
+local dst = curmd<defmd and dll or dllpath..dll
+local fr <close> = io.open(src, 'rb')
 if not fr then goto continue end
-fw = io.open(dll, 'wb')
+local fw <close> = io.open(dst, 'wb')
 if not fw then goto continue end
 fw:write(fr:read('a'))
-fw:close()
-fr:close()
-print('Copied '..dll)
+print(string.format('Copied %s to %s', src, dst))
 ::continue:: end
